fde4ec601ab856e05554ba8b0934c72d63347678
Maksim <Maks_Sannikov@bk.ru>
2021-10-15 21:31:14 +0500

    review code

--- Movie_MVVM/Movie_MVVM/Core/AppDelegate.swift
+++ Movie_MVVM/Movie_MVVM/Core/AppDelegate.swift
@@ -5,12 +5,4 @@ import RealmSwift
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first)
-        return true
-    }
-}
+class AppDelegate: UIResponder, UIApplicationDelegate {}
